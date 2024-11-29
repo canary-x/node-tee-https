21e2322dc36fb5fa48f9928751085b49bb1ad386
Martin <MartinBechtle@users.noreply.github.com>
2024-11-29 12:42:45 +0100

    wip

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server running on port ${port}`);
 });
-
